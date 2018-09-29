e016c928e6af43c5a60142b7b6910d8c83ac644f
grey-zeng <zengqinxiong@outlook.com>
2018-09-29 23:20:06 +0800

    第101题

---
+++ cpp/101.SymmetricTree/solution.h
@@ -0,0 +1,17 @@
+#include "../util/treeNode.h"
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;   
+        return isSymmetric(root->left, root->right);
+    }
+
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (left && right) {
+            return left->val == right->val && isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left); 
+        } else {
+            return !left && !right;
+        } 
+    }
+};
