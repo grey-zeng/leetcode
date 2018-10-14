1487de498186c0252e98ebb67d8406641da00a0b
grey-zeng <zengqinxiong@outlook.com>
2018-10-14 20:27:42 +0800

    补充

--- cpp/114.FlattenBinaryTreeToLinkedList/solution.h
+++ cpp/114.FlattenBinaryTreeToLinkedList/solution.h
@@ -2,8 +2,20 @@
 
 class Solution {
 public:
-    // 把先序遍历转成链表，那就使用逆序遍历每次都拿到最后一个节点再使用指针连起来
     void flatten(TreeNode* root) {
+        // 常规的遍历做法
+        while(root) {
+            if (root->left) {
+                TreeNode* prev = root->left;
+                while (prev->right) prev = prev->right;
+                prev->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+            }
+            root = root->right;
+        }
+        return;
+        // 把先序遍历转成链表，那就使用逆序遍历每次都拿到最后一个节点再使用指针连起来
         if (root == NULL) return;
         flatten(root->right);
         flatten(root->left);
